05144af2b5df088400da29b20a86304235a6d751
ahmedsayedsa <ahmedsayedsa@users.noreply.github.com>
2025-08-27 02:02:54 +0300

    اختياريه

--- config/settings.py
+++ config/settings.py
@@ -2,13 +2,14 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 from functools import lru_cache
 import os
+from typing import Optional
 
 class Settings(BaseSettings):
     port: int = Field(8080, env="PORT")
-    jwt_secret: str = Field(..., env="JWT_SECRET")
+    jwt_secret: Optional[str] = Field(None, env="JWT_SECRET")  # جعلها اختيارية
     firebase_project_id: str = Field("chrome-backbone-458221-m9", env="FIREBASE_PROJECT_ID")
     firebase_client_email: str = Field("firebase-adminsdk@chrome-backbone-458221-m9.iam.gserviceaccount.com", env="FIREBASE_CLIENT_EMAIL")
-    firebase_private_key: str = Field(..., env="FIREBASE_PRIVATE_KEY")
+    firebase_private_key: Optional[str] = Field(None, env="FIREBASE_PRIVATE_KEY")  # جعلها اختيارية
     
     class Config:
         env_file = ".env"
